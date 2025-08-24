3f66a64ab938e2c029b1551fcb23214b1fee7f9b
ikhlasdansantai <initesting5@gmail.com>
2025-08-24 07:21:16 +0700

    feat: Add utility function for calculating discounted total price

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getDiscountPrice(price: number, discount: number) {
+  const discountAmount = price * (discount / 100);
+  const finalPrice = price - discountAmount;
+
+  return parseFloat(finalPrice.toFixed(2));
 }
