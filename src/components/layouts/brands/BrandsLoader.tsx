fae3f403fa80bbeef57f7c7ad62c0a5e31fd126c
ikhlasdansantai <initesting5@gmail.com>
2025-08-24 07:50:12 +0700

    feat: adjusting brands loader margin-top

--- src/components/layouts/brands/BrandsLoader.tsx
+++ src/components/layouts/brands/BrandsLoader.tsx
@@ -1,6 +1,6 @@
 export default function BrandsLoader() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8 gap-y-12">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8 gap-y-12 mt-10 sm:mt-20">
       {Array.from({ length: 6 }, (_, i) => (
         <span key={i} className="w-full h-82 aspect-square bg-slate-300 animate-pulse rounded-md"></span>
       ))}
