78583b39532f5e96bcef45bf2d338270cff50ca5
ikhlasdansantai <initesting5@gmail.com>
2025-08-24 07:35:30 +0700

    feat: Add new Brands section

---
+++ src/components/sections/Brands.tsx
@@ -0,0 +1,18 @@
+import { useBrands } from "@/hooks/useBrands";
+import BrandsMain from "../layouts/brands/BrandsSection";
+import BrandError from "../layouts/brands/BrandError";
+import BrandsLoader from "../layouts/brands/BrandsLoader";
+
+export default function BrandsSection() {
+  const { brands, isPending, error } = useBrands();
+
+  if (error) return <BrandError />;
+  if (isPending) return <BrandsLoader />;
+  if (!brands) return <p>Tidak ada data</p>;
+
+  return (
+    <section className="mt-20">
+      <BrandsMain />
+    </section>
+  );
+}
